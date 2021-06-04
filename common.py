2ea7297cf69db5f44eb3107eda6b26b178026196
SeemlySplinter <85322091+SeemlySplinter@users.noreply.github.com>
2021-06-03 18:26:02 -0700

    Add files via upload
    Add current files for use in physics and math.

---
+++ common.py
@@ -0,0 +1,123 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import logistics as log
+import time
+from typing import List, Union
+
+
+def replica_job():
+    """ dummy-job, replica loop with time.sleep()"""
+
+    my_arr = np.zeros(75)
+    stop = len(my_arr)
+    msg = 'replicating job, '
+    # print(msg)
+    for i in range(stop):
+        time.sleep(0.1)
+        log.print_loop_progress(i, stop, msg=msg)
+
+
+def build_plot(
+    fig_label :       str                               ,
+    x_arrs :          List[List[float]]                 ,
+    y_arrs :          List[List[float]]                 ,
+    plot_labels :     List[str]                         ,
+
+    fig_loc :         List[float]           = [600, 250],
+    fig_size :        List[float]           = [640, 520],
+    x_label :         str                   = ''        ,
+    y_label :         str                   = ''        ,
+    plot_title :      str                   = ''        , 
+    linestyles :      List[str]             = None      , 
+    plot_colors :     List[str]             = None      , 
+    plot_markers :    List[str]             = None      , 
+    legend :          Union[bool, List]     = False     ,
+    axes :            Union[bool, List]     = False     ,
+    grid:             Union[bool, List]     = False     , 
+    y_lim :           List[float]           = None      ,
+    x_lim :           List[float]           = None      ,
+):
+    """ 
+    Builds a plot using matplotlib.pyplot;
+    "plt.show()" must be called elsewhere,
+    
+        (--required arguments--)
+
+        fig_label :       str                     ,
+        x_arrs :          List[List[float]]       ,
+        y_arrs :          List[List[float]]       ,
+        plot_labels :     List[str]               ,
+
+        (--optional arguments--)
+        
+        fig_loc :         [float, float]          ,
+        fig_size :        [float, float]          ,
+        x_label :         str                     ,
+        y_label :         str                     ,
+        plot_title :      str                     , 
+        linestyles :      List[str]               , 
+        plot_colors :     List[str]               , 
+        plot_markers :    List[str]               , 
+        legend :          bool | [bool, str]      ,
+        axes :            bool | [bool, str, str] ,
+        grid:             bool | [bool, float]    , 
+        x_lim :           [float, float]          ,
+        y_lim :           [float, float]          ,
+    """
+    
+    plt.figure(fig_label)
+
+    # Window geometry (type:int, units:px):
+    fig_x_pos, fig_y_pos = fig_loc
+    fig_width, fig_height = fig_size 
+    mgr = plt.get_current_fig_manager()
+    mgr.window.setGeometry(
+        fig_x_pos, fig_y_pos, fig_width, fig_height
+    )
+
+    # Main plot call:
+    for i in range(len(y_arrs)):
+        linestyle = linestyles[i] if linestyles else '-'
+        color = plot_colors[i] if plot_colors else f'C{i}'
+        marker = plot_markers[i] if plot_markers else None
+        plt.plot(
+            x_arrs[i], y_arrs[i], 
+            label=plot_labels[i],
+            linestyle=linestyle,
+            color=color, 
+            marker=marker
+        )
+    
+    # labels:
+    plt.xlabel(x_label)
+    plt.xlabel(y_label)
+    plt.title(plot_title)
+
+    # options
+    if legend:
+        legend_loc = legend[1] if type(legend) == list else 'best'
+        plt.legend(loc=legend_loc)
+    if grid:
+        opacity = grid[1] if type(grid) == list else 0.3
+        plt.grid(alpha=opacity)
+    if axes:
+        axes_linestyle = axes[1] if type(axes) == list else '--'
+        axes_color = axes[2] if type(axes) == list else 'k'
+        plt.axhline(0, linestyle=axes_linestyle, color=axes_color)
+        plt.axvline(0, linestyle=axes_linestyle, color=axes_color)
+    if x_lim:
+        plt.xlim(x_lim[0], x_lim[1])
+    if y_lim:
+        plt.ylim(y_lim[0], y_lim[1])
+
+
+def abs_max(arrs:List)->float:
+    """ Return the maximum from among the absolute values
+    of all elements within several arrays (of any dimension).
+    The arrays should be arranged along axis 0 of "arrs".  """
+
+    maxes = [np.abs(arr).max() for arr in arrs]
+    magnitude_max = np.amax(maxes)
+    
+    return magnitude_max
+
---
+++ logistics.py
@@ -0,0 +1,192 @@
+import numpy as np
+import matplotlib as mpl
+import time
+import sys
+import warnings
+from typing import List
+from functools import reduce
+
+
+if True:
+    # suppress reportUnboundVariable:
+    start_time = None 
+
+
+def print_version_info(dependencies:List[str], ):
+    """ "dependencies" should contain additional dependencies 
+    (beyond 'python') required to run the current script.  
+    e.g. ['numpy', 'matplotlib'] """
+    
+    head = 'versions:'
+    prefix = ' ' * (len(head)+1)
+    py_vers_template = '3.x.x'
+    py_vers_length = len(py_vers_template)
+    
+    print(head + ' python--' + sys.version[:py_vers_length])
+    if 'numpy' in dependencies:
+        print(prefix + 'numpy--' + np.__version__)
+    if 'matplotlib' in dependencies:
+        print(prefix + 'matplotlib--' + mpl.__version__)
+
+
+def print_file_info(filename:str=None, location:str=None, ):
+    """ "filename" and "location" are both optional. 
+    "filename" should be the file basename. 
+    "location" should be the relative patt without the file 
+    basename, possibly with "~" for the HOME directory """
+
+    if location:
+        print(f'location: "{location}"') 
+    if filename:
+        print(f'filename: "{filename}"') 
+
+
+def seconds_to_timestr(t:float, full_str:bool=False):
+    """ converts a time "t" in fractional seconds into a
+    time-string of one of the following forms--
+
+        hh:mm:ss.ss if "full_str" == True ,
+        hh:mm:ss if (1 hour) <= "t" ,
+        mm:ss if (1 min) <= "t" <= (1 hour) ,
+        mm:ss.ss if "t" <= (1 min) . """
+
+    time_parts = reduce(
+        lambda part_tuple, divisor:
+            divmod(part_tuple[0], divisor) + part_tuple[1:], 
+            [(t * 100,), 100, 60, 60]
+    )
+    time_parts = (int(p) for p in time_parts)
+    h, m, s, cs = time_parts
+    
+    if full_str == True:
+        out = f'{h:02d}:{m:02d}:{s:02d}.{cs:02d}'
+
+    elif h >= 1:
+        out = f'{h:02d}:{m:02d}:{s:02d}'
+
+    elif m >= 1:
+        out = f':{m:02d}:{s:02d}'
+
+    else:
+        out = f'{m:02d}:{s:02d}.{cs:02d}'
+
+    return out
+
+
+def print_time_required(
+    init_time:float, 
+    buffer:bool=False, 
+    msg:str=None, 
+    full_str:bool=False, 
+):
+    """ "init_time" should be bound at start of 'stopwatch' 
+    using the time module,
+    "buffer" is an option to replace "msg" with an equal amount
+    of spaces (' '),
+    "msg" should a status string (printed elsewhere if buffer is 
+    False),
+    "full_str" is an option to fix the time format as 
+    hh:mm:ss.ss ."""
+
+    if buffer:
+        buffer_str = ' ' * (len(msg) + 1)
+        print(buffer_str, end='')
+
+    time_req = time.perf_counter() - init_time
+    time_str = seconds_to_timestr(time_req, full_str=full_str)
+
+    print(f'time required: {time_str:s}')
+
+
+def print_total_time(start_time:float, full_str:bool=False):
+    """ "start_time" should be bound at start of script using 
+    the time module, then passed into this routine, 
+    "full_str" is an option to fix the time format as 
+    hh:mm:ss.ss . """
+    
+    elapsed_time = time.perf_counter() - start_time
+    time_str = seconds_to_timestr(elapsed_time, full_str=full_str)
+    print(f'runtime: {time_str:s}')
+
+
+def housekeeping_initial(
+    ignore_warnings:bool=False, 
+    location:str=None,
+    filename:str=None,
+    print_version:bool=False, 
+    dependencies:List[str]=None,
+):
+    """ "ignore_warnings" is used to quiet all warnings from the
+    terminal. 
+    "location" should be the relative path of the current file 
+    (without basename). 
+    "filename" should be the name of the current file. 
+    "print_version" is used to send version info to stdout; if this 
+    option is True, then one must provide strings of required 
+    dependencies in "dependency_list", e.g. ['numpy', 'matplotlib'] 
+    """
+    global start_time
+    
+    line = '=' * 40
+    start_time = time.perf_counter()
+
+    if ignore_warnings:
+        warnings.filterwarnings('ignore')
+    
+    print()
+    if print_version:
+        print_version_info(dependencies=dependencies)
+    print_file_info(filename=filename, location=location)
+    print('--NEW RUN--')
+    print(line)
+    print()
+    
+
+def housekeeping_final(
+    location:str=None,
+    filename:str=None,
+    print_timing:bool=False,
+):
+    """ "location" should be the relative path of the current file 
+    (without basename). 
+    "filename" shoudld be the name of the current file. 
+    "print_timing" is used to print the total runtime for the file.
+    """
+    
+    line = '=' * 40
+
+    print()
+    print(line)
+    print_file_info(filename=filename, location=location) 
+    if print_timing:
+        print_total_time(
+            start_time=start_time, full_str=False
+        )
+    print('--DONE--')
+    print('\n')
+    
+
+def print_loop_progress(
+    index:int, 
+    index_max:int, 
+    msg:str=None, 
+    buffer:bool=False, 
+):
+    """ Print "msg" to terminal if "buffer" is False, else n spaces 
+    where n is the length of "msg"; 
+    print the progress for ("index" + 1) 
+    steps out of "index_max" total steps; 
+    flush stdout while printing.  """
+
+    percent_complete = 100 * (index+1) / index_max
+    buffer_str = ' ' * (len(msg) + 1)
+    prefix = '\r' + buffer_str if buffer else '\r' + msg
+    
+    print(
+        prefix + f'progress: {percent_complete:0.0f}%',
+        flush=True, end=''
+    )
+    if index + 1 == index_max: # condition for last step in loop
+        print() 
+
+           
